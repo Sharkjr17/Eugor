6209431efbf6d27cecb41368c6374966d33cfbf5
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2025-08-24 17:01:49 +0000

    modified:   dungeon.json 	modified:   item.json 	modified:   level.json 	modified:   main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import random
 import sys, subprocess
 import math
 import json
+from colorama import just_fix_windows_console
+from termcolor import colored
+#from blessings import Terminal
+#from rich import print
+
+
 with open('data.json', 'r') as file:
     data = json.load(file)
 with open('enemy.json', 'r') as file:
@@ -17,6 +23,7 @@ with open('level.json', 'r') as file:
 i = None
 difficulty = None   #difficulty, first asked variable
 Stats = {}
+inv = []
 pathWeight = [] #create pathWeight
 for a in list(level.keys()):  pathWeight.append(level[a]["weight"]) #add weights from level.json to pathWeight
 
@@ -29,53 +36,58 @@ def move():
     pathChoices = random.randint(2, 5)  #how many places can the player go to?
     pathView = random.choices(list(level.keys()), weights = pathWeight, k = pathChoices)    #choose the actual paths for the amount of path choices
     
-    while valid == False:   #loop, player decides where to go in overworld
+    while valid != True:   #loop, player decides where to go in overworld
         for a in range(pathChoices):    #display path choices with descriptions
             print(f"{a+1}.) {pathView[a]}: {level[pathView[a]]["description"]}\n")
-        playerChoice = input(f"Select path (1-{pathChoices}) -->")  #input
+        playerChoice = int(input(f"Select path (1-{pathChoices}) -->"))  #input
         match playerChoice: #check how many path choices there are and allow valid number inputs for pathChoices
-            case "1":
-                print("chose path 1")
-                valid = True
-            case "2":
-                print("chose path 2")
-                valid = True
-            case "3" if pathChoices >= 3:
-                print("chose path 3")
-                valid = True
-            case "4" if pathChoices >= 4:
-                print("chose path 4")
-                valid = True
-            case "5" if pathChoices >= 5:
-                print("chose path 5")
-                valid = True
+            case 1: valid = True
+            case 2: valid = True
+            case 3 if pathChoices >= 3: valid = True
+            case 4 if pathChoices >= 4: valid = True
+            case 5 if pathChoices >= 5: valid = True
         subprocess.run('clear', shell=True)
     
-    match level[pathView[int(playerChoice) - 1]]["type"]:
-        case "dungeon": dungeon(level[pathView[int(playerChoice) - 1]])
-        case "buff": buff(level[pathView[int(playerChoice) - 1]])
-        case "boss": boss(level[pathView[int(playerChoice) - 1]])
+    match level[pathView[playerChoice - 1]]["type"]:
+        case "dung": dung(pathView[playerChoice - 1])
+        case "buff": buff(pathView[playerChoice - 1])
+        case "shop": shop(pathView[playerChoice - 1])
+        case "boss": boss(pathView[playerChoice - 1])
+
         
         
 
 
 
 
-def dungeon():
-    print("dungeon")
+def dung(alevel):
+    print(alevel)
     
     
     
     
+def buff(alevel):
+    valid = False
+    match alevel:
+        case "Smithery":
+            print("a")
+        case "Church":
+            print("b")
+
+
+def shop(alevel):
+    print(alevel)
     
-def buff():
-    print("buff")
     
     
-    
-    
-def boss():
-    print("boss")
+def boss(alevel):
+    print(alevel)
+
+
+def fight(enemy):
+    print("fa")
+
+
 
 
 
@@ -94,17 +106,23 @@ def run():
             case "A":
                 difficulty = "easy"
                 Stats.update([("maxHP", 500), ("HP", 500), ("strengthMult", 2)])
+                inv.append(item["Copper Sword"])
             case "B":
                 difficulty = "intermediate"
                 Stats.update([("maxHP", 500), ("HP", 500), ("strengthMult", 1.25)])
+                inv.append(item["Copper Sword"])
             case "C":
                 difficulty = "hard"
                 Stats.update([("maxHP", 250), ("HP", 250), ("strengthMult", 1)])
+                inv.append(item["Copper Sword"])
             case "D":
                 difficulty = "impossible"
                 Stats.update([("maxHP", 100), ("HP", 100), ("strengthMult", 0.5)])
+                inv.append(item["Copper Dagger"])
         subprocess.run('clear', shell=True)
     print(data["Intro Text 1"][difficulty]) #Import text from data json
+    print(Stats)
+    print(inv)
     move()
 
 
